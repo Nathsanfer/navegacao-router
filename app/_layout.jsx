26b9564270fa94c7ae0a6cdf72de4156ef813a39
Nathalia Santos Ferreira <nathalia.s.ferreira9@aluno.senai.br>
2025-03-27 11:38:11 -0300

    feat: Create new folders e files

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{title: 'Home'}}/>
+            <Stack.Screen name="about" options={{title: 'Sobre'}}/>
+            <Stack.Screen name="(aux)/termos" options={{title: 'Termos de Uso'}}/>
+        </Stack>
+    )
+    }
---
+++ app/index.jsx
@@ -0,0 +1,43 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Home() {
+
+    return (
+        <View style={styles.container}>
+
+            <Link href="/about" asChild >
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText}>Bora para a Sobre!!</Text>
+                </Pressable>
+            </Link>
+
+            <Link href="/termos">Leia os termos</Link>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+    button: {
+        backgroundColor: '#007bff',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        textAlign: 'center'
+    }
+});
